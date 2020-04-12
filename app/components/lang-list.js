c26e0ce9ab2a61214b33c627ddb0eac7ebe05107
James <Jamesmaye6094@gmail.com>
2020-04-12 21:14:27 +0800

    adding new Route and css styles

--- app/components/lang-list.js
+++ app/components/lang-list.js
@@ -5,8 +5,7 @@ export default class LangListComponent extends Component {
 
     
     showSelection(){
-
-         
+  
         var radios = document.querySelectorAll("li .radios");
         var choice = "";
         
@@ -14,10 +13,33 @@ export default class LangListComponent extends Component {
             
             if(radios[i].checked == true){
                 choice = radios[i].value;
+            }else{
+
             }
         }
 
-        return alert("You have selected " + choice);
+        return alert("You have selected to learn " + choice);
+
+    }
+
+    selected(){
+        var radios = document.querySelectorAll("li .radios");
+        var choice = "";
+        
+        for(var i = 0; i < radios.length; i++){
+            
+            if(radios[i].checked == true){
+                choice = radios[i].value;
+                radios[i].parentNode.style.color = "rgb(229, 255, 0)";
+                radios[i].parentNode.style.backgroundColor ="rgb(129, 129, 129)"
+            }else{
+                radios[i].parentNode.style.color = "black";
+
+                radios[i].parentNode.style.backgroundColor ="#333"
+
+
+            }
+        }
 
     }
 }
